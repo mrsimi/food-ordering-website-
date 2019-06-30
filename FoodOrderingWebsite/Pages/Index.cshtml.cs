28148a059df41c73c2844eea134e2532d3a51f75
Simi <adegokesimi@gmail.com>
2019-06-30 19:28:54 +0100

    Edited for dropdown on searchlocations

--- FoodOrderingWebsite/Pages/Index.cshtml.cs
+++ FoodOrderingWebsite/Pages/Index.cshtml.cs
@@ -5,10 +5,13 @@ using System.Threading.Tasks;
 using FoodOrderingWebsite.Models;
 using FoodOrderingWebsite.Services;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace FoodOrderingWebsite.Pages
 {
+    [ValidateAntiForgeryToken]
     public class IndexModel : PageModel
     {
         private readonly ISearchService _searchService;
@@ -20,17 +23,25 @@ namespace FoodOrderingWebsite.Pages
 
         public IEnumerable<Resturant> ResturantList { get; set; } 
         public IEnumerable<Food> FoodList { get; set; }
-        
+
+        [BindProperty]
+        public int SelectedLocation { get; set; }
+        public SelectList LocationOptions { get; set; }
+       
+
+        //hardcoded location for testing
+        string[] resturantsLocation = new string[] { "Akure", "Lagos" };
 
         public async Task OnGetAsync()
         {
-            //hardcoded location for testing
-            string[] resturantsLocation = new string[] { "Akure", "Lagos"};
+            
             Random random = new Random();
             string randomLocation = resturantsLocation[random.Next(resturantsLocation.Count())];
 
             var resturants = await _searchService.GetResturantsByLocation(randomLocation);
             ResturantList = resturants;
+
+            LocationOptions = new SelectList(resturantsLocation, randomLocation);
 
             ViewData["FoodSearch"] = "";
             ViewData["Location"] = randomLocation;
@@ -43,11 +54,14 @@ namespace FoodOrderingWebsite.Pages
             var searchLocation = Request.Form["searchLocationParams"];
             var searchFood = Request.Form["searchFoodParams"];
 
+            LocationOptions = new SelectList(resturantsLocation, searchLocation);
+
             //Console.WriteLine($"{searchFood}, {searchLocation} ");
             if (String.IsNullOrEmpty(searchFood))
             {
                 var resturantsByLocation = await _searchService.GetResturantsByLocation(searchLocation);
                 ResturantList = resturantsByLocation;
+
                 
                 ViewData["Location"] = searchLocation;
             }
