63ff2bd5cdcce0e3af2a94373a550ef9b6f9a706
Simi <adegokesimi@gmail.com>
2019-07-08 12:09:01 +0100

    Added complete order function to checkout page

--- FoodOrderingWebsite/Pages/Checkout.cshtml.cs
+++ FoodOrderingWebsite/Pages/Checkout.cshtml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using FoodOrderingWebsite.Models;
+using FoodOrderingWebsite.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -11,7 +12,14 @@ namespace FoodOrderingWebsite.Pages
     public class CheckoutModel : PageModel
     {
         public Cart Cart { get; set; }
-        public void OnPost(int Id, string foodName, decimal foodPrice, string resturantName)
+        public int resturant { get; set; }
+        private readonly ICartService _cartService;
+
+        public CheckoutModel(ICartService cartService)
+        {
+            _cartService = cartService;
+        }
+        public void OnPost(int Id, string foodName, decimal foodPrice, string resturantName, int resturantId)
         {
             Cart = new Cart()
             {
@@ -21,6 +29,23 @@ namespace FoodOrderingWebsite.Pages
                 UserName = "Simi"
             };
 
+            resturant = resturantId;
+
+        }
+
+        public async Task OnPostCompleteOrder(string foodBought, decimal totalPrice, int resturantId, string resturantName)
+        {
+            var deliveryStatus = new DeliveryStatus()
+            {
+                FoodBought = foodBought,
+                Price = totalPrice,
+                EstimatedTimeofArrival = _cartService.GetETA("somelocation", resturantId),
+                UserName = "simi"
+            };
+
+            await _cartService.AddToDeliveryStatusDb(deliveryStatus);
+
+            OnPost(0,foodBought, totalPrice,resturantName, resturantId);
         }
     }
 }
